b041053ce3991d4bbd970758473736a3c53ca17a
anubhavjha08 <anubhavjha0812@gmail.com>
2024-10-08 17:43:54 +0530

    08OCT-2

--- src/components/dashboard/Header.jsx
+++ src/components/dashboard/Header.jsx
@@ -1,20 +1,50 @@
-import { History } from 'lucide-react';
-import React from 'react'
+import React from 'react';
+import { BellIcon, ChevronDown, History, Plus, Settings, Users } from 'lucide-react';
+import SearchInput from './SearchInput';
 
 const Header = () => {
     return (
-        <nav>
-
-            <div>
-                <button>
-                    <History/>
+        <nav className="bg-gray-200 h-12 flex items-center justify-between px-8">
+            <div className="flex gap-3">
+                <button aria-label="View History">
+                    <History className="w-5 h-5" />
                 </button>
+
+                <SearchInput />
             </div>
 
-            <div></div>
+            <div className="flex items-center gap-3">
+                {/* plus icon */}
+                <div className="pl-3 pr-3 border-l border-gray-300">
+                    <button className="p-1 bg-blue-500 rounded-md" aria-label="Add New">
+                        <Plus className="text-slate-50 w-4 h-4" />
+                    </button>
+                </div>
 
+                <div className="flex space-x-2">
+                    <button className="p-1 hover:bg-blue-500 rounded-lg" aria-label="View Users">
+                        <Users className="w-4 h-4" />
+                    </button>
+
+                    <button className="p-1 hover:bg-blue-500 rounded-lg" aria-label="View Notifications">
+                        <BellIcon className="w-4 h-4" />
+                    </button>
+
+                    <button className="p-1 hover:bg-blue-500 rounded-lg" aria-label="Settings">
+                        <Settings className="w-4 h-4" />
+                    </button>
+                </div>
+
+                {/* User profile section */}
+                <div className="flex items-center">
+                    <button className="flex items-center gap-1" aria-label="User Profile">
+                        <span>Anubhav</span>
+                        <ChevronDown className="w-3 h-3" />
+                    </button>
+                </div>
+            </div>
         </nav>
     );
 }
 
-export default Header
+export default Header;
---
+++ src/components/dashboard/SearchInput.jsx
@@ -0,0 +1,12 @@
+import { Search } from 'lucide-react';
+import React from 'react'
+
+const SearchInput = () => {
+    return (
+        <div>
+            <Search className="w-5 h-5" />
+        </div>
+    );
+}
+
+export default SearchInput
